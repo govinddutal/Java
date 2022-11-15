e60463fc763e9fe7635d27fb0d99e853545d8ff3
hp <hp@DESKTOP-AO3862A>
2022-11-15 18:49:00 +0530

    code

--- Maven4/src/test/java/Test/Class1.java
+++ Maven4/src/test/java/Test/Class1.java
@@ -2,6 +2,6 @@ package Test;
 
 public class Class1 {
 public static void main(String[] args) {
-	System.out.println("i am busy right now");
+	System.out.println("kay zadi kay dongar kay hotel sagal kay ok made  ");
 }
 }
