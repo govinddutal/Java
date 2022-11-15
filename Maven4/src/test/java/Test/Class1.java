2290e32a477f68c2b3d4a1c04e8689f79360074c
hp <hp@DESKTOP-AO3862A>
2022-11-15 18:22:21 +0530

    new code

--- Maven4/src/test/java/Test/Class1.java
+++ Maven4/src/test/java/Test/Class1.java
@@ -2,12 +2,6 @@ package Test;
 
 public class Class1 {
 public static void main(String[] args) {
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	System.out.println("TC01");
-	
-	
+	System.out.println("i am busy right now");
 }
 }
