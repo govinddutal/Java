01556d4f9ee7aa27690141e3050b5891c9cfa0bd
govinddutal <118239794+govinddutal@users.noreply.github.com>
2022-11-15 19:05:02 +0530

    Update Class1.java

--- Maven4/src/test/java/Test/Class1.java
+++ Maven4/src/test/java/Test/Class1.java
@@ -2,6 +2,6 @@ package Test;
 
 public class Class1 {
 public static void main(String[] args) {
-	System.out.println("kay zadi kay dongar kay hotel sagal kay ok made  ");
+	System.out.println("kay zadi kay dongar kay hotel sagal kay ok made ahe ami sadya ghuhati la ahot ");
 }
 }
