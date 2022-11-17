2b1bc5b822fef72ea0c725665ce4d59b90d90a61
hp <hp@DESKTOP-AO3862A>
2022-11-17 17:01:21 +0530

    Class2 required

--- Maven4/src/test/java/Test/Class1.java
+++ Maven4/src/test/java/Test/Class1.java
@@ -3,5 +3,12 @@ package Test;
 public class Class1 {
 public static void main(String[] args) {
 	System.out.println("kay zadi kay dongar kay hotel sagal kay ok made ahe ami sadya ghuhati la ahot ");
+	
+System.out.println("TC02");	
+System.out.println("TC03");	
+System.out.println("TC04");	
+System.out.println("TC05");	
+System.out.println("TC06");	
+System.out.println("TC08");	
 }
 }
